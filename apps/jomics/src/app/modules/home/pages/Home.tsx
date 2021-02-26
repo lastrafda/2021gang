d05f3de38ff6ac5dcd336a9d59c4ce87f06ea49b
José Lastra <lastrafda@gmail.com>
2021-02-25 22:03:39 -0300

    add basic structure: Home & whatHero

---
+++ apps/jomics/src/app/modules/home/pages/Home.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from 'react';
+
+const Home: FC = () => {
+  return <>Hola</>;
+};
+
+export default Home;
---
+++ apps/jomics/src/app/modules/whatHero/pages/WhatHero.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from 'react';
+
+const Home: FC = () => {
+  return <>What Hero</>;
+};
+
+export default Home;
